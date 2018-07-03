565f1d8e80d6032dc4cd0a590ef186e53c080ad2
maddogg91 <40213091+maddogg91@users.noreply.github.com>
2018-07-03 13:26:52 -0400

    Add files via upload

---
+++ guild.py
@@ -0,0 +1,18 @@
+import random
+
+guild= random.randint(1,5)
+
+if guild == 1:
+    print("You have been selected to the AERO guild")
+
+elif guild == 2:
+    print("You have been selected to the Celestial guild")
+
+elif guild == 3:
+    print("You have been selected to the Flame guild")
+
+elif guild == 4:
+    print("You have been selected to the Mist guild")
+
+elif guild == 5:
+    print("You have been selected to the Terra guild")
